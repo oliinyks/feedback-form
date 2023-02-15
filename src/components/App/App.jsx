9f4b4f4ecaf67e3d72e17e0236a9611525349b19
svitlana.oliinyk <101010916+oliinyks@users.noreply.github.com>
2023-02-15 15:45:43 +0200

    add main files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,8 @@
-import { Route, Routes } from 'react-router-dom';
-import { lazy } from 'react';
-import { SharedLayout } from '../SharedLayout/SharedLayout';
-import { NotFound } from 'pages/NotFound/NotFound';
-
-const Home = lazy(() => import("../../pages/Home"));
-const Movies = lazy(() => import("../../pages/Movies"));
-const MovieDetails = lazy(() => import("../../pages/MovieDetails"));
-const Cast = lazy(() => import("../../pages/Cast/Cast"));
-const Reviews = lazy(() => import("../../pages/Reviews/Reviews"));
 
 export const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
-          <Route path="*" element={<NotFound/>} />
-        </Route>
-      </Routes>
+    Hi
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import {BrowserRouter} from 'react-router-dom';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './constants/theme';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-	<BrowserRouter basename="/movies">
-	<ThemeProvider theme={theme}>
-    <App />
-	</ThemeProvider>
-	</BrowserRouter>
+    <BrowserRouter basename="/feedback-form">
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
